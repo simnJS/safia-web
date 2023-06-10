9f242511ef0d8f8b033fbf72a08e7ab9994f136d
simnJS <simnkrystal@gmail.com>
2023-06-11 01:05:10 +0200

    Updated Meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+
+      <Head>
+        <title>Safia</title>
+        <meta name="description" content="Safia is a discord bot that allows you to manage your server easily and quickly." />
+        <meta name="keywords" content="discord, bot, safia, safiabot, safia bot, safia discord bot, discord bot, discord bot safia, safia discord, safia bot discord, safia bot discord invite, safia bot discord link, safia bot discord invite link, safia bot discord server, safia bot discord support, safia bot discord support server, safia bot discord server invite, safia bot discord server link, safia bot discord server invite link, safia bot discord server support, safia bot discord server support server, safia bot discord server support invite, safia bot discord server support link, safia bot discord server support invite link, safia bot discord server support server invite, safia bot discord server support server link, safia bot discord server support server invite link, safia bot discord server support server link invite, safia bot discord server support server invite link, safia bot discord server support server link invite link" />
+        <meta name="author" content="SimnJS" />
+        </Head>
       <body>
         <Main />
         <NextScript />
