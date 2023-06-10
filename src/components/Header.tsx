4ad0de52832727e941e8c0ccb6b244d40d89c9d5
simnJS <simnkrystal@gmail.com>
2023-06-11 00:47:01 +0200

    Updated logo href

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,9 @@ export default function Header() {
   return (
     <header className={styles.header}>
         <div className='flex-1'>
-            <img className={styles.img} src="/logo.png" alt="Logo" />
+        <a href="/" className="footer-link">
+            <img className={styles.img} src="/logo.png" alt="Logo"/>
+        </a>
         </div>
         <h1 className={styles.h1}>SAFIA</h1>
         <div className="flex-1 flex justify-end">
